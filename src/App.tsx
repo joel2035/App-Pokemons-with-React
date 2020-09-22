b13a15ebaec8d6d63b5081e963de8fc9eab73904
joel2035 <joel.pokam@hetic.net>
2020-09-22 10:28:52 +0200

    add pokemons card

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,29 @@ const App: FunctionComponent = () => {
     },[])
 
     return (
-        <h1>il y'a, {pokemons.length} !</h1>
+        <div>
+            <h1 className="center">Pokedex</h1>
+            <div className="container">
+                <div className="row">
+                    {pokemons.map(({id, name, picture, created}) =>
+                    <div className="col s6 m4" key={id}>
+                        <div className="card horizontal">
+                            <div className="card-image">
+                                <img src={picture} alt={name}/>
+                            </div>
+                            <div className="card-stactked">
+                                <div className="card-content">
+                                    <p>{name}</p>
+                                    <p><small>{created.toString()}</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    )}
+                </div>
+            </div>
+        </div>
+       
     )
 }
 export default App;
